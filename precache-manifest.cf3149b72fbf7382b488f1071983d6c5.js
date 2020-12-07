af95835c6c4479a6eafe1ff979622dab64f0f8cf
DRAKOS <darkostius2@gmail.com>
2020-12-07 15:15:59 +0200

    Updates

---
+++ precache-manifest.cf3149b72fbf7382b488f1071983d6c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fad692f3f5eb89161949bbb1031b9b7",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "228bfa99d6f3610f0c03",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ff67f3b4.chunk.css"
+  },
+  {
+    "revision": "2e23161ad69179c19bb8",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0b076d13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0b076d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228bfa99d6f3610f0c03",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1ef829e3.chunk.js"
+  },
+  {
+    "revision": "b58a435a92ac135c88f5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.82cfee65.js"
+  }
+]);
