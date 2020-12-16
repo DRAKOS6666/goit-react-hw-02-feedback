08c18c765a914f7c82758681fcb0e1361d85dfc1
DRAKOS <drakostius2@gmail.com>
2020-12-16 22:04:52 +0200

    Updates

---
+++ precache-manifest.ba2b8ea61bb427815752cdd279418ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420a2810535c20a61bf151bf53971b7b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "3fa83ffd5f642e9acdbd",
+    "url": "/goit-react-hw-02-feedback/static/css/main.ff67f3b4.chunk.css"
+  },
+  {
+    "revision": "0be0b8ebd4c4fa0d4022",
+    "url": "/goit-react-hw-02-feedback/static/js/2.39ba89f0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-02-feedback/static/js/2.39ba89f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa83ffd5f642e9acdbd",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f29e0c51.chunk.js"
+  },
+  {
+    "revision": "b58a435a92ac135c88f5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.82cfee65.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.1df83ea4c367de6356d8d7102f5eacdb.js"
+  "/goit-react-hw-02-feedback/precache-manifest.ba2b8ea61bb427815752cdd279418ceb.js"
 );
 
 self.addEventListener('message', (event) => {
